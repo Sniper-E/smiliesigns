614f4aaaaa463e7141f0933b69fccc340d9ef0cb
Sniper-E <ehumphrey57@gmail.com>
2016-10-20 13:32:29 -0500

    Changed soft requirement to >=3.1.2-RC1

--- ext.php
+++ ext.php
@@ -22,6 +22,6 @@ class ext extends \phpbb\extension\base
 	public function is_enableable()
 	{
 		$config = $this->container->get('config');
-		return version_compare($config['version'], '>=3.1.9');
+		return version_compare($config['version'], '>=3.1.2-RC1');
 	}
 }
