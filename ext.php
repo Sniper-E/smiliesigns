ba85e15d0640897fb6ee303a0b1b6acac88de8cd
Sniper-E <ehumphrey57@gmail.com>
2016-10-19 22:42:29 -0500

    1.0.8 Update

---
+++ ext.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* @package Smilie Signs
+* @copyright (c) 2015 Sniper_E - http://sniper-e.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+
+namespace sniper\smiliesigns;
+
+/**
+* Extension class for custom enable/disable/purge actions
+*/
+
+class ext extends \phpbb\extension\base
+{
+	/**
+	* Enable extension if phpBB version requirement is met
+	*
+	* @return bool
+	* @access public
+	*/
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return version_compare($config['version'], '3.1.9', '>=');
+	}
+}
